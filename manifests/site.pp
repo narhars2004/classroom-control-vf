0ebc4b30fe56ffa7840fb3c202d6a31a54b890be
Troy Klein <spidersddd@users.noreply.github.com>
2016-09-27 13:03:26 -0500

    Lab 7.1

--- manifests/site.pp
+++ manifests/site.pp
@@ -43,4 +43,10 @@ node default {
   # Example:
   #   class { 'my_class': }
   notify { "Hello, my name is ${::fqdn}": }
+  file { '/etc/motd':
+    ensure  => file,
+    owner   => 'root',
+    group   => 'root',
+    content => "Puppet class is fun!\n", 
+  }
 }
