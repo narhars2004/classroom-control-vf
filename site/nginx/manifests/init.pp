a0ac1b0195449076c010bb3b56041e788c8c3dfb
Troy Klein <troy.klein@puppetlabs.com>
2016-09-28 07:47:13 -0500

    added new nginx module for Lab 11.2

---
+++ site/nginx/manifests/init.pp
@@ -0,0 +1,28 @@
+class nginx {
+  package { 'nginx':
+    ensure => present,
+  }
+
+  file { '/var/www':
+    ensure  => directory,
+    require => Package['nginx'],
+  }
+
+  file { '/var/www/index.html':
+    ensure => file,
+    source => 'puppet:///modules/nginx/index.html',
+  }
+
+  file { '/etc/nginx/nginx.conf':
+    ensure  => file,
+    source  => 'puppet:///modules/nginx/nginx.conf',
+    require => Package['nginx'],
+  }
+
+  file { '/etc/nginx/conf.d/default.conf':
+    ensure  => file,
+    source  => 'puppet:///modules/nginx/default.conf',
+    require => Package['nginx'],
+  }
+
+}
