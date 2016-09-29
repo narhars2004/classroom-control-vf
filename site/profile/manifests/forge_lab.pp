2be0ae4dc3c529258e58e5a0d0b2e2796cc71a9f
Troy Klein <troy.klein@puppetlabs.com>
2016-09-29 13:20:38 -0500

    profile wrapper for 19.1

---
+++ site/profile/manifests/forge_lab.pp
@@ -0,0 +1,23 @@
+class profile::forge_lab {
+  $redis_password = hiera('profile::redis_password', 'default_password')
+
+  sysctl { "kernel.sysrq":
+    ensure    => present,
+    comment => "",
+  }
+
+  include epel
+  class { 'limits': }
+   limits::limits { 'fundamentals_nofile':
+    ensure     => present,
+    user       => 'fundamentals',
+    limit_type => 'nofile',
+    hard       => 16384,
+    soft       => 16384,
+  }
+
+  class { 'redis':
+    bind        => $::ipaddress,
+    masterauth  => $redis_password,
+  }
+}
