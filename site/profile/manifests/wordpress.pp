28789c29419af8dad053608612b2424272428f8d
Troy Klein <troy.klein@puppetlabs.com>
2016-09-30 09:34:48 -0500

    added IP to master as domain

--- site/profile/manifests/wordpress.pp
+++ site/profile/manifests/wordpress.pp
@@ -1,5 +1,9 @@
 class profile::wordpress {
+  $domain = hiera('profile::wordpress::domain', '54.218.5.21')
+
   class { 'wordpress':
-    install_dir => '/var/www/html',
+    install_dir    => '/var/www/html',
+    wp_multisite   => true,
+    wp_site_domain => $domain,
   }
 }
