f6bfa9e8a7948da5d103067702777bc89e0c3c77
Troy Klein <troy.klein@puppetlabs.com>
2016-09-30 10:01:49 -0500

    added mpm_module => 'prefork'

--- site/profile/manifests/apache.pp
+++ site/profile/manifests/apache.pp
@@ -2,10 +2,10 @@ class profile::apache {
   $docroot = hiera('profile::apache::docroot', '/opt/wordpress')
   
   apache::vhost { $::fqdn:
-    port            => 80,
-    docroot         => $docroot,
+    port       => 80,
+    docroot    => $docroot,
+    mpm_module => 'prefork',
   }
 
   include ::apache::mod::php
-  include ::apache::mod::prefork
 }
