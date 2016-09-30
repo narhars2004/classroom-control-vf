d07ab1e0544f93e9bc2078e26f69a0d32beacb03
Troy Klein <troy.klein@puppetlabs.com>
2016-09-30 13:23:13 -0500

    cleanup

--- site/profile/manifests/wordpress.pp
+++ site/profile/manifests/wordpress.pp
@@ -1,10 +1,8 @@
 class profile::wordpress {
-  #  $domain = hiera('profile::wordpress::domain', '54.218.5.21')
 
   class { 'wordpress':
-    install_dir    => '/var/www/html',
-    wp_multisite   => false,
-    #wp_site_domain => $domain,
-    wp_siteurl     => 'http://54.218.5.21/port/4004',
+    install_dir  => '/var/www/html',
+    wp_multisite => false,
+    wp_siteurl   => 'http://54.218.5.21/port/4004',
   }
 }
