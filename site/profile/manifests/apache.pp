9256de52a212d5b5a00886cf35cdb31bbcc8ac87
Troy Klein <troy.klein@puppetlabs.com>
2016-09-30 10:17:04 -0500

    no vhost

--- site/profile/manifests/apache.pp
+++ site/profile/manifests/apache.pp
@@ -2,10 +2,5 @@ class profile::apache {
   $docroot = hiera('profile::apache::docroot', '/opt/wordpress')
 
   include ::apache
-  apache::vhost { $::fqdn:
-    port       => 80,
-    docroot    => $docroot,
-  }
-
   include ::apache::mod::php
 }
--- site/profile/manifests/wordpress.pp
+++ site/profile/manifests/wordpress.pp
@@ -2,7 +2,7 @@ class profile::wordpress {
   #  $domain = hiera('profile::wordpress::domain', '54.218.5.21')
 
   class { 'wordpress':
-    install_dir    => '/opt/wordpress',
+    install_dir    => '/var/www/html',
     wp_multisite   => false,
     #wp_site_domain => $domain,
   }
