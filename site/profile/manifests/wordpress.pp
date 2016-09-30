a4aab9186891faed05e4e4a98a55307e63ee8ce1
Troy Klein <troy.klein@puppetlabs.com>
2016-09-30 09:45:03 -0500

    change wordpress install location

--- site/profile/manifests/wordpress.pp
+++ site/profile/manifests/wordpress.pp
@@ -2,7 +2,7 @@ class profile::wordpress {
   #  $domain = hiera('profile::wordpress::domain', '54.218.5.21')
 
   class { 'wordpress':
-    install_dir    => '/var/www/html',
+    install_dir    => '/opt/wordpress',
     #  wp_multisite   => true,
     #wp_site_domain => $domain,
   }
